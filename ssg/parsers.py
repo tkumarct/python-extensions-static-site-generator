991b8fd0072fc05fd5c0e5c9372d98840c09f3ce
tkumarct <tkumar@ctepl.com>
2023-04-13 10:28:41 +0530

    module3

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,63 +1,48 @@
-import shutil
-import sys
-
-from typing import List
-from pathlib import Path
-
-from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
+
+from ssg import hooks
 
 
 class Parser:
     base_ext = ".html"
     file_exts: List[str] = []
-
     def valid_file_ext(self, file_ext):
         return file_ext in self.file_exts
-
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
-
     def read(self, path):
         with open(path, "r") as file:
             return file.read()
-
     def write(self, path, dest, content):
         file_path = dest / path.with_suffix(self.base_ext).name
         with open(file_path, "w") as file:
             file.write(content)
-
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
-
-
 class ResourceParser(Parser):
     file_exts = [".jpg", ".png", ".gif", ".css", ".html"]
-
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
-
-
 class MarkdownParser(Parser):
     file_exts = [".md", ".markdown"]
-
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
-
-
 class ReStructuredTextParser(Parser):
     file_exts = [".rst"]
-
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
--- ssg/site.py
+++ ssg/site.py
@@ -1,16 +1,39 @@
 import sys
 from pathlib import Path
 
-from ssg import extensions
+from ssg import extensions, hooks
 
 
 class Site:
     def __init__(self, source, dest, parsers=None):
-@@ -27,6 +29,7 @@ def run_parser(self, path):
+        self.source = Path(source)
+        self.dest = Path(dest)
+        self.parsers = parsers or []
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
+    def load_parser(self, ext):
+        for parser in self.parsers:
+            if parser.valid_file_ext(ext):
+                return parser
+    def run_parser(self, path):
+        parser = self.load_parser(path.suffix)
+        if parser is not None:
+            parser.parse(path, self.source, self.dest)
+        else:
+            self.error(
+                "No parser for the {} extension, file skipped!".format(path.suffix)
             )
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
