005454cf660eb0adc39a6586db30d5e5b9abe6e1
tkumarct <tkumar@ctepl.com>
2023-04-13 10:25:00 +0530

    module2

--- ssg/site.py
+++ ssg/site.py
@@ -6,23 +6,7 @@ from ssg import extensions
 
 class Site:
     def __init__(self, source, dest, parsers=None):
-        self.source = Path(source)
-        self.dest = Path(dest)
-        self.parsers = parsers or []
-    def create_dir(self, path):
-        directory = self.dest / path.relative_to(self.source)
-        directory.mkdir(parents=True, exist_ok=True)
-    def load_parser(self, ext):
-        for parser in self.parsers:
-            if parser.valid_file_ext(ext):
-                return parser
-    def run_parser(self, path):
-        parser = self.load_parser(path.suffix)
-        if parser is not None:
-            parser.parse(path, self.source, self.dest)
-        else:
-            self.error(
-                "No parser for the {} extension, file skipped!".format(path.suffix)
+@@ -27,6 +29,7 @@ def run_parser(self, path):
             )
 
     def build(self):
@@ -30,9 +14,3 @@ class Site:
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
-    @staticmethod
-    def error(message):
-        sys.stderr.write("\x1b[1;31m{}\n".format(message))
